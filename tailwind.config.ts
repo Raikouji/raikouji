9a1f8de6759e820e68357b9c0b269a81dda5df5b
takna <tak_na@icloud.com>
2023-09-18 21:38:02 +0900

    Add: npm tailwind 公式プラグイン4つ

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,14 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  corePlugins: {
+    aspectRatio: false,
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
 export default config
