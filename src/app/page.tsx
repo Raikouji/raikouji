2f3f85ecfd7bb8ae06c4c244364dad2face4a268
takna <tak_na@icloud.com>
2023-09-22 16:57:51 +0900

    Add: ホームページ用の CSS Modules を追加設定

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import styles from './homepage.module.css'
+
 export default function Home() {
   return (
-    <div className='grow'>
+    <div className={styles.wrapper}>
       <h1>Hello Next!</h1>
       <a href='#' className='btn btn-primary'>
         ボタン
