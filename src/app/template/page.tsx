d5a664b3e511b6ac337199ef68c4a5eb071ab05f
takna <tak_na@icloud.com>
2023-09-22 17:09:09 +0900

    Add: テンプレートページ用の CSS Modules を追加設定

--- src/app/template/page.tsx
+++ src/app/template/page.tsx
@@ -1,5 +1,6 @@
 import Card from '@/components/Card'
 import type { Metadata } from 'next'
+import styles from './template.module.css'
 
 export const metadata: Metadata = {
   title: '子ページのタイトルです',
@@ -8,9 +9,9 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <>
+    <div className={styles.wrapper}>
       <h1>Hello, Next! template/page.tsx</h1>
       <Card />
-    </>
+    </div>
   )
 }
