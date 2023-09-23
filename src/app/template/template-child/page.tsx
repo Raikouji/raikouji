11c1c0b2aaf95b7275014c6e563c0717328c9f01
takna <tak_na@icloud.com>
2023-09-23 18:06:18 +0900

    Add: サンプル用孫ページの実装

---
+++ src/app/template/template-child/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: '孫ページのタイトルです',
+  description: '孫ページのディスクリプションです',
+}
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello, Next! template/template-child/page.tsx</h1>
+      <p>子ページのサンプル</p>
+    </div>
+  )
+}
