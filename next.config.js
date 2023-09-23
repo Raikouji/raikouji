f9e4b6fe15813f7a5f2fb2ace81fc0a8abe1f5c6
takna <tak_na@icloud.com>
2023-09-23 15:34:30 +0900

    Add: npm @next/bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -2,3 +2,12 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+
+/**
+ * Webpack Bundle Analyzer (@next/bundle-analyzer)
+ */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({})
