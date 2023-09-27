d5aeb0ac1d938427b5b97060f5fb54fe93bb216e
takna <tak_na@icloud.com>
2023-09-27 15:00:46 +0900

    Add: サンプル用 Tailwind Grid

--- src/app/template/page.tsx
+++ src/app/template/page.tsx
@@ -11,6 +11,23 @@ export default function Page() {
   return (
     <div className={styles.wrapper}>
       <h1>Hello, Next! template/page.tsx</h1>
+      {/*Tailwind css Grid 12columns*/}
+      <div className='grid grid-cols-12 gap-4'>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+        <div className='col bg-gray-200'>col</div>
+      </div>
       <Card>
         <p>
           ★★ダミーコピーです手はおっかさんの演奏硝子屋をセロに思ったばこだた。
