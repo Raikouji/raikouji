8b34e2b7477cbd37fc7bb48bace3efb7a7215229
takna <tak_na@icloud.com>
2023-09-27 18:47:05 +0900

    Add: ヒーロー画像に文字ブロック追加

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -4,16 +4,22 @@ import styles from './Hero.module.css'
 function Hero() {
   return (
     <div className={styles.wrapper}>
-      <div className='aspect-h-3 aspect-w-2 md:aspect-h-9 md:aspect-w-16'>
-        <Image
-          src='/lorem.jpg'
-          alt='Lorem'
-          width={2100}
-          height={1400}
-          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
-          style={{ objectFit: 'cover' }}
-          priority
-        />
+      <div className='relative'>
+        <div className='aspect-h-3 aspect-w-2 relative md:aspect-h-9 md:aspect-w-16'>
+          <Image
+            src='/lorem.jpg'
+            alt='Lorem'
+            width={2100}
+            height={1400}
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            style={{ objectFit: 'cover' }}
+            priority
+          />
+        </div>
+
+        <div className='absolute top-1/2 block w-1/2 bg-amber-200'>
+          <p>あああああ</p>
+        </div>
       </div>
     </div>
   )
