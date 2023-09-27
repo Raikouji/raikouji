46bbd532e3c57267a70d03031be17306840a3846
takna <tak_na@icloud.com>
2023-09-27 17:21:38 +0900

    Add: container をデフォルトで中央揃えに

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+    },
     fontFamily: {
       sans: ['var(--font-notoSansJP)'],
       serif: [
