648f7eea696e26bd126b5e352eb87a903a5733bc
takna <tak_na@icloud.com>
2023-09-27 17:58:07 +0900

    Add: ヒーロー画像のアスペクト比指定

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,10 +1,20 @@
+import Image from 'next/image'
 import styles from './Hero.module.css'
 
 function Hero() {
   return (
     <div className={styles.wrapper}>
-      <p>ヒーロー画像</p>
-      <p>です。</p>
+      <div className='aspect-h-3 aspect-w-2 md:aspect-h-9 md:aspect-w-16'>
+        <Image
+          src='/lorem.jpg'
+          alt='Lorem'
+          width={2100}
+          height={1400}
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+          style={{ objectFit: 'cover' }}
+          priority
+        />
+      </div>
     </div>
   )
 }
