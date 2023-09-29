6103c913751bf1d8e0943fa297ccdf77003dde8c
takna <tak_na@icloud.com>
2023-09-30 05:30:02 +0900

    Add: prettier に TailwindCSS の設定

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  trailingComma: "all",
+  trailingComma: 'all',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
   jsxSingleQuote: true,
+  tailwindFunctions: ['clsx'],
   plugins: [
-    "prettier-plugin-organize-imports",
-    import("prettier-plugin-tailwindcss"),
+    'prettier-plugin-organize-imports',
+    import('prettier-plugin-tailwindcss'),
   ],
-};
+}
