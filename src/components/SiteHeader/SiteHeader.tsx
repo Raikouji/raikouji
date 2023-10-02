be8c9d2ffc74eef43b4337dfc1f18214681e20ef
takna <tak_na@icloud.com>
2023-10-02 14:08:30 +0900

    Refactor: ヘッダー

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -4,13 +4,13 @@ import MainNavigation from '@/components/MainNavigation'
 
 function SiteHeader() {
   return (
-    <div className='header-wrapper bg-white'>
-      <header className='${styles.wrapper} mx-auto my-3 flex w-full max-w-[1140px] flex-row items-center justify-between'>
+    <>
+      <header className='container flex flex-row justify-between border border-amber-600 py-8'>
         <Logo tagline='タグラインです' />
         <MainNavigation />
       </header>
       <Breadcrumb />
-    </div>
+    </>
   )
 }
 
