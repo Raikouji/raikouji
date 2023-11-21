cacfec16929a49289348a7e92c991262a8ca63cd
takna <tak_na@icloud.com>
2023-11-21 11:00:04 +0900

    Add: Animation during page transitions

---
+++ src/app/template.tsx
@@ -0,0 +1,24 @@
+'use client'
+import { motion } from 'framer-motion'
+import React from 'react'
+
+const variants = {
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
+}
+
+export default function Template({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.main
+      variants={variants}
+      initial='hidden'
+      animate='enter'
+      transition={{
+        type: 'linear',
+        duration: 2,
+      }}
+    >
+      {children}
+    </motion.main>
+  )
+}
