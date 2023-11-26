718db46d7ef7b03766bc080a0708933a6f219ced
takna <tak_na@icloud.com>
2023-11-27 02:03:47 +0900

    Update: About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -39,9 +39,11 @@ export default function Page() {
               <h2 className='mt-4 text-center font-serif'>住職 ごあいさつ</h2>
               <div className='mt-8 flex flex-col items-center gap-4 md:flex-row md:gap-8 lg:gap-12'>
                 <p>
-                  <img
-                    src='https://picsum.photos/id/2/400/600.webp'
+                  <Image
+                    src='/images/portrait.jpg'
                     alt='頼光寺 住職の近影'
+                    width={384}
+                    height={256}
                   />
                 </p>
                 <div className='md:w-3/4'>
@@ -73,9 +75,9 @@ export default function Page() {
               <ImageAboveHeading />
             </p>
             <h2 className='mt-4 text-center'>頼光寺の沿革</h2>
-            <div className='aspect-h-3 aspect-w-2 relative mt-3 w-full md:aspect-h-4 md:aspect-w-16'>
+            <div className='aspect-h-3 aspect-w-2 relative mt-4 w-full md:aspect-h-4 md:aspect-w-16'>
               <Image
-                src='/images/lorem.jpg'
+                src='/images/spider.jpg'
                 alt='Lorem'
                 width={2100}
                 height={1400}
@@ -85,52 +87,95 @@ export default function Page() {
               />
             </div>
             <div className='mx-auto max-w-screen-md'>
-              <p className='mt-6'>
-                祥雲山頼光寺は、一條天皇長保年間(紀元1000年頃)に、源満仲公令夫人
-                法如尼公の願いにより設立されました。設立者は源頼光公の四男で、開山（初代住職）は源頼光公の四男永寿阿闍梨でした。
-              </p>
-              <p className='mt-3'>
-                源頼光公(治安元年(1021年)七月二十四日
-                寂)は神儀を行い、霊像を安置し、亡くなった後の自身の安らかな世界を祈る寺として祥雲山頼光寺を設立しました(真言律宗に所属)。
+              <p className='mt-8 font-bold'>
+                頼光寺は、源頼光公の信仰深さから始まり、時代の変遷と共に衰退と復興を繰り返してきた歴史あるお寺です。
+                紀元1000年頃の設立以来多くの試練を経て、現在は曹洞宗に所属し「北摂七福神寿老人
+                あじさい寺」の名で親しまれています。
               </p>
             </div>
+
             <div className='mx-auto max-w-screen-lg'>
-              <div className='mt-12 flex flex-col gap-10'>
-                <div className='mx-auto flex flex-col items-center lg:flex-row lg:gap-8'>
-                  <p className=''>
-                    <img src='https://picsum.photos/id/2/384/256.webp' alt='' />
+              <div className='mt-12 flex flex-col gap-16'>
+                <div className='mx-auto flex flex-col items-center lg:flex-row lg:gap-12'>
+                  <p className='order-1 lg:w-5/12'>
+                    <Image
+                      src='/images/toumbs.jpg'
+                      alt=''
+                      width={2000}
+                      height={1333}
+                    />
                   </p>
-                  <div className='w-3/4'>
-                    <p>
-                      現代の急激な都市化にともない、核家族の増加、未婚者の増加、出生率の低下等、様々な要因により、祭祀後継者の絶えるご家族が増えてゆく傾向にある今日、すでにこの問題に当面して悩んでおられる方も多いかと思います。
-                      そういった方々のご要望にお応えして、当頼光寺では当寺境内にある頼光寺
-                      園内に永代供養共同墓を建立いたしました。
+                  <div className='lg:w-7/12'>
+                    <h3 className='text-center text-primary'>頼光寺の設立</h3>
+                    <p className='mt-3 leading-loose'>
+                      祥雲山頼光寺は、一條天皇長保年間(紀元1000年頃)に、源満仲公令夫人
+                      法如尼公（ほうにょにこう）の願いにより設立されました。設立者は源頼光公の四男で、開山（初代住職）は源頼光公の四男永寿阿闍梨でした。
+                    </p>
+                    <p className='mt-3 leading-loose'>
+                      源頼光公(治安元年(1021年)七月二十四日
+                      寂)は神儀を行い、霊像を安置し、亡くなった後の自身の安らかな世界を祈る寺として祥雲山頼光寺を設立しました(真言律宗に所属)。
+                    </p>
+                  </div>
+                </div>
+
+                <div className='mx-auto flex flex-col items-center lg:flex-row lg:gap-12'>
+                  <p className='lg:w-5/12'>
+                    <Image
+                      src='/images/lotus.jpg'
+                      alt=''
+                      width={2000}
+                      height={1333}
+                    />
+                  </p>
+                  <div className='lg:w-7/12'>
+                    <h3 className='text-center text-primary'>衰退と復興</h3>
+                    <p className='mt-3 leading-loose'>
+                      二世となる永覚阿闍梨が亡くなった後、平安時代、鎌倉時代、戦国時代の各時代に度々戦争により衰退しましたが、
+                      1671年(霊元天皇寛文十一年)に禅僧万愚和尚が寺を復興しました。更に1746年(桜町天皇延享三年)に、
+                      現在の亀岡市にある臨済宗の法常寺の末寺となり、1804年(光格天皇文化元年)に公式に認められ、
+                      尼寺（女性だけの寺）となりました。
+                    </p>
+                  </div>
+                </div>
+
+                <div className='mx-auto flex flex-col items-center lg:flex-row lg:gap-12'>
+                  <p className='order-1 lg:w-5/12'>
+                    <Image
+                      src='/images/altar.jpg'
+                      alt=''
+                      width={2000}
+                      height={1333}
+                    />
+                  </p>
+                  <div className='lg:w-7/12'>
+                    <h3 className='text-center text-primary'>
+                      継承・お寺の維持
+                    </h3>
+                    <p className='mt-3 leading-loose'>
+                      840年(天保十一年)には、本寺の十世勅謚
+                      普照恵燈禅師は、ここを弟子たちの坐禅の道場とし、
+                      優れた者を選んで住持を継がせていきました。しかし、1875年(明治八年)には、
+                      当時の状況の中で寺を維持するのが難しくなり、本寺である法常寺に合併されました。
+                      それでも、旧信徒らの希望により、建物や仏像、その他の物々はそのまま引き継がれ、
+                      現在も本堂内に安置されています。
                     </p>
                   </div>
                 </div>
 
                 <div className='mx-auto flex flex-col items-center lg:flex-row lg:gap-8'>
-                  <p className='order-1'>
-                    <img src='https://picsum.photos/id/2/384/256.webp' alt='' />
+                  <p className='lg:w-5/12'>
+                    <Image
+                      src='/images/temple-and-ajisai.jpg'
+                      alt=''
+                      width={2000}
+                      height={1333}
+                    />
                   </p>
-                  <div className='w-3/4'>
-                    <p>
-                      現代の急激な都市化にともない、核家族の増加、未婚者の増加、出生率の低下等、様々な要因により、祭祀後継者の絶えるご家族が増えてゆく傾向にある今日、すでにこの問題に当面して悩んでおられる方も多いかと思います。
-                      そういった方々のご要望にお応えして、当頼光寺では当寺境内にある頼光寺
-                      園内に永代供養共同墓を建立いたしました。
-                    </p>
-                  </div>
-                </div>
-
-                <div className='mx-auto flex flex-col items-center lg:flex-row lg:gap-8'>
-                  <p className=''>
-                    <img src='https://picsum.photos/id/2/384/256.webp' alt='' />
-                  </p>
-                  <div className='w-3/4'>
-                    <p>
-                      現代の急激な都市化にともない、核家族の増加、未婚者の増加、出生率の低下等、様々な要因により、祭祀後継者の絶えるご家族が増えてゆく傾向にある今日、すでにこの問題に当面して悩んでおられる方も多いかと思います。
-                      そういった方々のご要望にお応えして、当頼光寺では当寺境内にある頼光寺
-                      園内に永代供養共同墓を建立いたしました。
+                  <div className='lg:w-7/12'>
+                    <h3 className='text-center text-primary'>現在</h3>
+                    <p className='mt-3 leading-loose'>
+                      1990年(平成二年)以来、頼光寺は曹洞宗に所属し、「北摂七福神寿老人
+                      あじさい寺」として親しまれています。
                     </p>
                   </div>
                 </div>
