052c533b5c9f3b11780a6d8c489b2845a89a4f8e
takna <tak_na@icloud.com>
2023-11-30 09:10:42 +0900

    Update: SiteHeader component logo.

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -8,12 +8,12 @@ function SiteHeader() {
       <header className='container flex max-w-screen-xl justify-between py-4 lg:items-center'>
         <div className='shrink-0'>
           {/*<p className='text-xs'>タグライン</p>*/}
-          <div className='flex items-center gap-4'>
+          <div className='flex items-center gap-2'>
             <Image
-              src='/images/lotus-shape.png'
-              alt='寺紋'
-              width={48}
-              height={48}
+              src='/images/ajisai-site-title.png'
+              alt='あじさい寺'
+              width={77}
+              height={52}
             />
             <h1 className='flex items-center gap-3 font-serif'>
               <span className='tracking-widest'>曹洞宗 祥雲山</span>
