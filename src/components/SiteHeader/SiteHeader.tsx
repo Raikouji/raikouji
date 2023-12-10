6b81599a2009bce9993d5d93de2e08b7713a54c8
takna <tak_na@icloud.com>
2023-12-10 17:31:23 +0900

    Add Link wrapper to site header in SiteHeader component

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -1,6 +1,7 @@
 // TODO: サイトタイトルをコンポーネントにする (Logo.tsx 途中まで作成)
 import MainNavigation from '@/components/MainNavigation'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function SiteHeader() {
   return (
@@ -8,18 +9,20 @@ function SiteHeader() {
       <header className='container flex max-w-screen-xl justify-between py-4 lg:items-center'>
         <div className='shrink-0'>
           {/*<p className='text-xs'>タグライン</p>*/}
-          <div className='flex items-center gap-2'>
-            <Image
-              src='/images/ajisai-site-title.png'
-              alt='あじさい寺'
-              width={77}
-              height={52}
-            />
-            <h1 className='flex items-center gap-3 font-serif'>
-              <span className='tracking-widest'>曹洞宗 祥雲山</span>
-              <span className='text-3xl tracking-[.25em]'>頼光寺</span>
-            </h1>
-          </div>
+          <Link href='/' className='lg:hover:opacity-80'>
+            <div className='flex items-center gap-2'>
+              <Image
+                src='/images/ajisai-site-title.png'
+                alt='あじさい寺'
+                width={77}
+                height={52}
+              />
+              <h1 className='flex items-center gap-3 font-serif'>
+                <span className='tracking-widest'>曹洞宗 祥雲山</span>
+                <span className='text-3xl tracking-[.25em]'>頼光寺</span>
+              </h1>
+            </div>
+          </Link>
         </div>
         <MainNavigation />
       </header>
