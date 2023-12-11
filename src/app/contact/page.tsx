313e7e510cfdfd87437b86146b133d819e1491a3
takna <tak_na@icloud.com>
2023-12-11 09:22:05 +0900

    Update 'Contact' page layout and content
    Significantly restructured 'Contact' page and improved textual information. The update focuses on how inquiries can be made, emphasizing on not supporting direct calls or emails but encouraging contact via forms. It also improves on the information about the offerings and introduces a FAQ section.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,6 +5,7 @@ import Card from '@/components/ui/Card'
 import { Button } from '@/components/ui/button'
 import { cn, outputMetadata } from '@/utils'
 import type { Metadata } from 'next'
+import Link from 'next/link'
 
 // for metadata
 const pageTitle = 'お問合せ'
@@ -65,43 +66,20 @@ export default function Page() {
                   お問合せについて
                 </h2>
                 <p className='mt-4'>
-                  ★★ダミーコピーですダミーコピーです手はおっかさんの演奏硝子屋をセロに思ったばこだた。それから思わ口まし勝たしはでまた箱のダミすて、みんなまでぶ弾いとだまし。
+                  電話やメール等での直接の問い合わせへの対応は行っておりません。
+                  開花状況については、別途お知らせ版をご覧いただくようお願い申し上げます。
+                  どうぞよろしくお願い申し上げます。
                 </p>
                 <p className='mt-4 text-center'>
-                  <Button>あじさいの開花状況を見る</Button>
+                  <Button asChild>
+                    <Link href='#'>あじさいの開花状況を見る</Link>
+                  </Button>
                 </p>
               </Card>
             </div>
           </section>
 
           <div className='flex flex-col gap-4 md:flex-row md:gap-8 lg:gap-20'>
-            <section className='lg:w-2/5'>
-              <div className='flex flex-col gap-8'>
-                <div>
-                  <p className='text-center'>
-                    <ImageAboveHeading imageName='uguisu' />
-                  </p>
-                  <h2 id='tel' className='mt-3 text-center font-serif'>
-                    電話でのお問合せ
-                  </h2>
-                </div>
-                <div>
-                  <p>
-                    電話でのお問合せの説明。★★ダミーコピーですダミーコピーです手はおっかさんの演奏硝子屋をセロに思ったばこだた。それから思わ口まし勝た
-                  </p>
-                  <p className='mt-3 text-sm font-bold text-red-500'>
-                    ※
-                    電話をおかけの際は今一度番号をお確かめの上、お掛け間違いには十分ご注意いただくようお願いします。
-                  </p>
-                </div>
-                <p className='text-center font-bold'>
-                  Tel. <span className='text-3xl'>072-794-1165</span>
-                </p>
-              </div>
-            </section>
-
-            <hr className='line-with-three-dots lg:hidden' />
-
             <section className='lg:w-3/5'>
               <div className='flex flex-col gap-8'>
                 <div>
@@ -113,7 +91,11 @@ export default function Page() {
                   </h2>
                 </div>
                 <p>
-                  フォームでのお問合せの説明。★★ダミーコピーですダミーコピーです手はおっかさんの演奏硝子屋をセロに思ったばこだた。それから思わ口まし勝た
+                  状況やご質問内容により、返信にお時間をいただく場合がございますことご了承ください。
+                </p>
+                <p className='text-sm font-bold text-red-500'>
+                  ※
+                  送信後5日間の間に返信がない場合、送受信エラーの可能性があります。その場合、お手数ですが電話でのお問い合わせをお願いいたします。
                 </p>
                 <div>
                   <iframe
@@ -129,6 +111,92 @@ export default function Page() {
                 </div>
               </div>
             </section>
+
+            <hr className='line-with-three-dots lg:hidden' />
+            <div className='lg:w-2/5'>
+              <section>
+                <div className='flex flex-col gap-8'>
+                  <div>
+                    <p className='text-center'>
+                      <ImageAboveHeading imageName='uguisu' />
+                    </p>
+                    <h2 id='tel' className='mt-3 text-center font-serif'>
+                      電話でのお問合せ
+                    </h2>
+                  </div>
+                  <div>
+                    <p>
+                      お急ぎの場合には時間内にお電話にてお問い合わせください。お問合せ前に
+                      <a href='#faq' className='underline'>
+                        よくあるご質問
+                      </a>
+                      もご参照ください。
+                    </p>
+                    <p className='mt-3 text-sm font-bold text-red-500'>
+                      ※
+                      電話をおかけの際は今一度番号をお確かめの上、お掛け間違いには十分ご注意いただくようお願いします。
+                    </p>
+                  </div>
+                  <p className='text-center font-bold'>
+                    Tel. <span className='text-3xl'>072-794-1165</span>
+                  </p>
+                </div>
+              </section>
+
+              <hr className='line-with-three-dots my-12' />
+
+              <section>
+                <p className='text-center'>
+                  <ImageAboveHeading imageName='priest2' />
+                </p>
+                <h2 id='tel' className='mt-3 text-center font-serif'>
+                  よくいただく質問
+                </h2>
+                <p className='mt-6'>
+                  よくいただく質問をまとめました。
+                  <br />
+                  これ以外にご不明な点や聞きたいことがございましたら、お問合せよりご連絡ください。
+                </p>
+                <ul className='mt-4 flex flex-col gap-6'>
+                  <li>
+                    <h3 className='text-lg text-primary'>
+                      Q. あじさいの開花状況は？
+                    </h3>
+                    <p>
+                      開花状況については、別途お知らせ版をご覧いただくようお願い申し上げます。
+                    </p>
+                  </li>
+                  <li>
+                    <h3 className='text-lg text-primary'>
+                      Q. お墓の見学はできますか？
+                    </h3>
+                    <p>
+                      はい。お墓の見学は可能です。案内不要であれば、当寺境内にございますのでご見学ください。案内をご希望の方は、事前にお電話にてお問合せください。
+                    </p>
+                  </li>
+                  <li>
+                    <h3 className='text-lg text-primary'>
+                      Q. お墓の購入はできますか？
+                    </h3>
+                    <p>
+                      はい。お墓の購入は可能です。お墓の購入をご希望の方は、事前にお電話にてお問合せください。
+                    </p>
+                  </li>
+                  <li>
+                    <h3 className='text-lg text-primary'>
+                      Q. お墓の管理や清掃はどのように行われていますか？
+                    </h3>
+                    <p>お墓の管理は、頼光寺にて行っております。</p>
+                  </li>
+                  <li>
+                    <h3 className='text-lg text-primary'>
+                      Q. お墓のお参りができる時間帯を教えてください
+                    </h3>
+                    <p>いつでもお参りいただけます。</p>
+                  </li>
+                </ul>
+              </section>
+            </div>
           </div>
         </div>
       </main>
