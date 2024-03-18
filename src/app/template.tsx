83f8ff9008d344bb112af33497719d16cbf90132
takna <tak_na@icloud.com>
2024-03-19 07:39:42 +0900

    add: move to scroll top ボタン

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,13 +1,37 @@
 'use client'
 import { motion } from 'framer-motion'
-import React from 'react'
-
+import { ArrowUpToLine } from 'lucide-react'
+import React, { useEffect, useState } from 'react'
+import { animateScroll as scroll } from 'react-scroll'
+import VisuallyHidden from '../components/VisuallyHidden'
 const variants = {
   hidden: { opacity: 0 },
   enter: { opacity: 1 },
 }
 
 export default function Template({ children }: { children: React.ReactNode }) {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const scrollToTop = () => {
+    scroll.scrollToTop()
+  }
+
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.scrollY > 300) {
+        setIsVisible(true)
+      } else {
+        setIsVisible(false)
+      }
+    }
+
+    window.addEventListener('scroll', toggleVisibility)
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
   return (
     <motion.div
       className='site-wrapper'
@@ -20,6 +44,13 @@ export default function Template({ children }: { children: React.ReactNode }) {
       }}
     >
       {children}
+      <button
+        className={`fixed bottom-4 right-0 z-50 cursor-pointer bg-primary-400/75 p-3 text-white transition-opacity hover:bg-primary-500 ${isVisible ? 'opacity-100' : 'opacity-0'}`}
+        onClick={scrollToTop}
+      >
+        <ArrowUpToLine />
+        <VisuallyHidden>ページトップへ移動</VisuallyHidden>
+      </button>
     </motion.div>
   )
 }
