ea4d0436bbfffd57e5fa44a9c7dca148c321fb5b
takna <tak_na@icloud.com>
2024-03-27 09:57:58 +0900

    Add: ドットインジゲーター、スタイル調整

--- src/app/test-carousel/page.tsx
+++ src/app/test-carousel/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 
+// TODO:
+//  ドットボタン：https://codesandbox.io/p/sandbox/embla-carousel-generator-react-7cgfq9?file=%2Fsrc%2Fjs%2FEmblaCarouselDotButton.tsx%3A16%2C10
+
 import Autoplay from 'embla-carousel-autoplay'
 import * as React from 'react'
 
@@ -13,13 +16,14 @@ import {
 } from '@/components/ui/carousel'
 
 import { type CarouselApi } from '@/components/ui/carousel'
+import { range } from '@/utils'
 
 const photos = [
-  'https://source.unsplash.com/1600x900/?nature,water',
-  'https://source.unsplash.com/1600x900/?nature,sky',
-  'https://source.unsplash.com/1600x900/?nature,forest',
-  'https://source.unsplash.com/1600x900/?nature,tree',
-  'https://source.unsplash.com/1600x900/?nature,flower',
+  { url: 'https://source.unsplash.com/1600x900/?nature,water', alt: 'aaaa' },
+  { url: 'https://source.unsplash.com/1600x900/?nature,sky', alt: 'bbbb' },
+  { url: 'https://source.unsplash.com/1600x900/?nature,forest', alt: 'cccc' },
+  { url: 'https://source.unsplash.com/1600x900/?nature,tree', alt: 'dddd' },
+  { url: 'https://source.unsplash.com/1600x900/?nature,flower', alt: 'eeee' },
 ]
 
 export default function CarouselPlugin() {
@@ -45,10 +49,10 @@ export default function CarouselPlugin() {
   )
 
   return (
-    <div className={`container flex max-w-screen-xl flex-col gap-12`}>
+    <div className={`container mx-auto max-w-screen-lg`}>
       <Carousel
         plugins={[plugin.current]}
-        className='mx-auto w-full max-w-screen-lg'
+        className='w-full overflow-hidden' // relative
         onMouseEnter={plugin.current.stop}
         onMouseLeave={plugin.current.reset}
         opts={{
@@ -57,23 +61,44 @@ export default function CarouselPlugin() {
         }}
         setApi={setApi}
       >
-        <CarouselContent>
-          {photos.map((photo, index) => (
-            <CarouselItem key={index}>
-              <div className='p-1'>
-                <Card className='aspect-square flex items-center justify-center border p-6'>
-                  <img src={photo} alt='' />
-                </Card>
-              </div>
+        <CarouselContent className='&>ml-0'>
+          {/* div.overflow-hidden > div.flex.-ml-4 */}
+          {photos.map(({ url, alt }, index) => (
+            <CarouselItem key={index} className='pl-0'>
+              {/* min-w-0 shrink-0 grow-0 basis-full, pl-4 */}
+              <Card className='flex items-center justify-center'>
+                <img src={url} alt={alt} />
+              </Card>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <p>
-          {current} / {count}
+        <p className='absolute bottom-2 w-full text-center'>
+          {CarouselDotIndicator({ count, current })}
         </p>
-        <CarouselPrevious />
-        <CarouselNext />
+        <CarouselPrevious className='left-2' />
+        {/* absolute h-8 w-8 rounded-full, -left-12 top-1/2 -translate-y-1/2 */}
+        <CarouselNext className='right-2' />
+        {/* absolute h-8 w-8 rounded-full, -right-12 top-1/2 -translate-y-1/2 */}
       </Carousel>
+    </div>
+  )
+}
+
+function CarouselDotIndicator({
+  count,
+  current,
+}: {
+  count: number
+  current: number
+}) {
+  return (
+    <div className='flex justify-center gap-2'>
+      {range(count).map((num) => (
+        <span
+          key={num}
+          className={`${num === current - 1 ? 'bg-primary-500' : 'bg-primary-200'} inline-block h-2 w-2 rounded-full opacity-60`}
+        />
+      ))}
     </div>
   )
 }
