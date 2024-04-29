850dee376af50172880805465f9bf3f1b7163136
takna <tak_na@icloud.com>
2024-04-29 10:53:53 +0900

    ブログページのUIとメタデータを更新
    ブログページのレイアウトとデザインを更新しました。メタデータの処理も追加しました。記事のラッパー、目次リンクなどのコンポーネントを導入し、よりユーザーフレンドリーなUIに変更しました。

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,6 +1,21 @@
-import { getBlogDetail, getBlogList } from '@/lib/microcms'
 import parse from 'html-react-parser'
+import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
+
+import { getBlogDetail, getBlogList } from '@/lib/microcms'
+import { outputMetadata } from '@/utils'
+
+import ArticleWrapper from '@/components/ArticleWrapper'
+import TocLink, { TocLinkItem } from '@/components/TocLink'
+
+// for metadata
+const pageTitle = '★★★★★★★★'
+const pageDescription = '★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★'
+
+export const metadata: Metadata = outputMetadata({
+  title: pageTitle,
+  description: pageDescription,
+})
 
 export async function generateStaticParams() {
   const { contents } = await getBlogList()
@@ -26,10 +41,21 @@ export default async function StaticDetailPage({
   }
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <main>{parse(post.content)}</main>
-      <p>{new Date().toLocaleString()}</p>
-    </div>
+    <ArticleWrapper pageTitle='お知らせ'>
+      <div className='mx-auto max-w-screen-md'>
+        <TocLink>
+          <TocLinkItem id='scarlet-seal'>御朱印</TocLinkItem>
+          <TocLinkItem id='wedding'>仏前結婚式</TocLinkItem>
+        </TocLink>
+      </div>
+
+      <h2>{post.title}</h2>
+      {parse(post.content)}
+
+      <p>Update: {new Date().toLocaleString()}</p>
+      <p>
+        <a href='../examples/microcms'>« 一覧に戻る</a>
+      </p>
+    </ArticleWrapper>
   )
 }
