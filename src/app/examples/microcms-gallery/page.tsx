efef112eef82f24e163f7b59b6272c88733c1173
takna <tak_na@icloud.com>
2024-05-03 15:10:47 +0900

    GalleryコンポーネントにPropsを追加
    Galleryコンポーネントに新たなPropsを追加し、列数(cols)の設定が可能になりました。これによりユーザーが表示する列数を制御できるようになりました。また、classNameとchildClassName propsも提供され、スタイルのカスタマイズが容易になりました。

--- src/app/examples/microcms-gallery/page.tsx
+++ src/app/examples/microcms-gallery/page.tsx
@@ -1,7 +1,33 @@
 import { getGalleryList } from '@/lib/microcms'
+import { cn } from '@/utils'
 import Image from 'next/image'
 
+type Props = {
+  cols?: number
+  colsMd?: number
+  colsLg?: number
+  colsXL?: number
+  className?: string
+  childClassName?: string
+}
+
 export default async function StaticPage() {
+  return (
+    <div>
+      <Gallery colsMd={3} colsLg={4} colsXL={5} />
+    </div>
+  )
+}
+
+async function Gallery({
+  cols = 2,
+  colsMd,
+  colsLg,
+  colsXL,
+  className = 'gap-2',
+  childClassName = 'mb-2',
+  ...delegated
+}: Props) {
   const { contents } = await getGalleryList({
     limit: 100,
     orders: 'system:default',
@@ -13,10 +39,19 @@ export default async function StaticPage() {
   }
 
   return (
-    <ul className='columns-2 gap-2 md:columns-3 lg:columns-4'>
+    <ul
+      className={cn(
+        `columns-${cols}`,
+        colsMd && `md:columns-${colsMd}`,
+        colsLg && `lg:columns-${colsLg}`,
+        colsLg && `lg:columns-${colsLg}`,
+        colsXL && `xl:columns-${colsXL}`,
+        className,
+      )}
+    >
       {contents.map((post) => {
         return (
-          <li key={post.id} className='mb-2'>
+          <li key={post.id} className={childClassName}>
             <figure className='break-inside-avoid border border-gray-100 shadow'>
               <Image
                 alt={post?.caption || ''}
