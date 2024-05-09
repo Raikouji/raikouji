0255884663bfeb839e7fd717f9f406beac7370b6
takna <tak_na@icloud.com>
2024-05-09 15:54:38 +0900

    Hrコンポーネント、クラス名と Props 対応するように
    Hrコンポーネントに新たにclassNameプロップスを追加しました。これにより、特定のHrコンポーネントに対してさらなるスタイリングが可能になります。また、任意のプロップスを渡せるようにdelegatedプロップスも追加しました。

--- src/components/Hr/Hr.tsx
+++ src/components/Hr/Hr.tsx
@@ -1,7 +1,15 @@
+import { cn } from '@/utils'
 import styles from './Hr.module.css'
 
-function Hr({ type }: { type?: 'square' }) {
-  return <hr className={type && styles[type]} />
+function Hr({
+  type,
+  className,
+  ...delegated
+}: {
+  type?: 'square'
+  className?: string
+}) {
+  return <hr className={cn(type && styles[type], className)} {...delegated} />
 }
 
 export default Hr
