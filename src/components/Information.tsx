a905974e37a802e4683d0e98461ea4a600ee0107
takna <tak_na@icloud.com>
2024-05-19 19:06:12 +0900

    Informationコンポーネントにデータ取得機能を追加
    Informationコンポーネントで基本情報の取得機能を追加しました。エラーハンドリングも実装して、データの取得に失敗した場合やデータが存在しない場合に適切に対応できるようにしました。また、画像情報の取得も行い、取得した画像を表示するように変更しました。

--- src/components/Information.tsx
+++ src/components/Information.tsx
@@ -1,7 +1,32 @@
+import { getBasicInformation } from '@/lib/microcms'
+import type { BasicInformation } from '@/types/post'
 import { cn } from '@/utils'
+import { format, parseISO } from 'date-fns'
 import Image from 'next/image'
+export default async function Information() {
+  let data: BasicInformation | null = null
+  let errorMassage = ''
 
-export default function Information() {
+  try {
+    data = await getBasicInformation()
+  } catch (error) {
+    errorMassage = `Error fetching basic information: ${error}`
+  }
+
+  if (errorMassage) {
+    return <p>{errorMassage}</p>
+  }
+
+  if (!data || !data.flowerPhoto || data.flowerPhoto.length === 0) {
+    return <p>No data or no flower photo.</p>
+  }
+
+  if (!data.isActive) {
+    return
+  }
+
+  const date = parseISO(data.publishedAt as string)
+
   return (
     <div
       className={cn(
@@ -22,28 +47,41 @@ export default function Information() {
       </h2>
       <div className='flex flex-col gap-8 lg:flex-row'>
         <div className='lg:order-1 lg:w-2/3'>
-          <h3 className='text-base'>
-            <a href='#' className='text-primary-dark underline'>
-              0月00日に「●●●●●●●●●●」を開催します
-            </a>
-          </h3>
-          <p className='mt-2 text-sm'>
-            ここに開花情報等を載せます。ダミーコピーです手はおっかさんの演奏硝子屋をセロに思ったばこだた。それから思わ口まし勝た。
-          </p>
-          <div className='mt-3 border border-primary p-4'>
-            <h3 className='text-base'>あじさいの開花状況 (2023-06-17)</h3>
+          <div className='-ml-6 w-fit rounded-3xl border bg-primary px-4 py-2 text-white'>
+            <h3 className='text-base'>
+              あじさいの開花状況 (更新：{format(date, 'MM月dd日 H時m分')})
+            </h3>
+            <p className='mt-1 text-sm'>{data.flowerState}</p>
+          </div>
+          <div>
+            <h3 className='mt-4 text-base'>
+              <a href='#' className='text-primary-dark underline'>
+                0月00日に「●●●●●●●●●●」を開催します
+              </a>
+            </h3>
+            <p className='mt-2 text-sm'>
+              ここに開花情報等を載せます。ダミーコピーです手はおっかさんの演奏硝子屋をセロに思ったばこだた。それから思わ口まし勝た。
+            </p>
+            <h3 className='mt-4 text-base'>
+              <a href='#' className='text-primary-dark underline'>
+                0月00日に「●●●●●●●●●●」を開催します
+              </a>
+            </h3>
             <p className='mt-2 text-sm'>
               ここに開花情報等を載せます。ダミーコピーです手はおっかさんの演奏硝子屋をセロに思ったばこだた。それから思わ口まし勝た。
             </p>
           </div>
         </div>
         <div className='lg:w-1/3'>
-          <Image
-            src='/images/ajisai2.jpg'
-            width={900}
-            height={600}
-            alt='あじさい'
-          />
+          <p className='aspect-square'>
+            <Image
+              src={data.flowerPhoto[0].url}
+              width={data.flowerPhoto[0].width}
+              height={data.flowerPhoto[0].height}
+              alt='開花状況 1'
+              className='h-60 w-60 rounded-lg object-cover shadow-lg'
+            />
+          </p>
         </div>
       </div>
     </div>
