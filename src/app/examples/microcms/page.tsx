f359954476403c17f956fd3e215b64a86ac7e861
takna <tak_na@icloud.com>
2024-05-21 07:46:53 +0900

    投稿日付の表示形式を変更
    投稿リストの表示形式を更新しました。各投稿について、ISO形式から「MM月dd日」形式に変換して表示するように変更しました。これにより、投稿日付が一目でわかるようになります。

--- src/app/examples/microcms/page.tsx
+++ src/app/examples/microcms/page.tsx
@@ -1,4 +1,5 @@
 import { getBlogList } from '@/lib/microcms'
+import { format, parseISO } from 'date-fns'
 import Link from 'next/link'
 
 export default async function StaticPage() {
@@ -14,14 +15,18 @@ export default async function StaticPage() {
       <p>Update: {time}</p>
       <ul>
         {contents.map((post) => {
+          const date = parseISO(post.publishedAt as string)
           return (
-            <li key={post.id}>
+            <li key={post.id} className='flex gap-1'>
               <Link
                 href={`/blog/${post.id}`}
                 className='text-blue-600 underline hover:text-blue-400'
               >
                 {post.title}
               </Link>
+              <span className='inline-block text-sm'>
+                {format(date, 'MM月dd日')}
+              </span>
             </li>
           )
         })}
