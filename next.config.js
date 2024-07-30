1dc93b001dc4529b71309d4ab6db82c038deaa1e
takna <tak_na@icloud.com>
2024-07-30 14:41:31 +0900

    next.config.js 画像読み込み設定のリファクタリング
    廃止になった "images.domains" を "images.remotePatterns" に変更。unsplash.com から画像を許可するパターンを追加。

--- next.config.js
+++ next.config.js
@@ -1,24 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // domains: ['images.microcms-assets.io'],
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.microcms-assets.io',
-        port: '',
-        pathname: '/assets/**',
-      },
-    ],
-    domains: ['source.unsplash.com', 'images.unsplash.com'],
-  },
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'images.microcms-assets.io',
+				port: '',
+				pathname: '/assets/**',
+			},
+			{
+				protocol: 'https',
+				hostname: '**.unsplash.com',
+				port: '',
+			},
+		],
+	},
 }
 
 /**
  * Webpack Bundle Analyzer (@next/bundle-analyzer)
  */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+	enabled: process.env.ANALYZE === 'true',
 })
 
 module.exports = withBundleAnalyzer(nextConfig)
