bea3cb52a30564c377618823c3c1752ed4633bba
takna <tak_na@icloud.com>
2024-08-02 13:40:49 +0900

    メインメニューの項目を定数化
    MainMenu.tsx で定義されていた項目を constants.ts に移動

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -7,27 +7,12 @@ import MainMenuMobile from '@/components/MainMenuMobile'
 import { useMediaQuery } from '@/hooks/use-media-query'
 
 import { BREAKPOINTS } from '@/constants'
+import { NAVIGATION_ITEMS } from '@/constants'
 
 export default function MainMenu() {
-	const items = [
-		{ href: '/', title: 'Home' },
-		{
-			href: 'about',
-			title: '頼光寺について',
-			subMenu: [
-				{ href: '/about', title: 'ごあいさつ・沿革' },
-				{ href: '/gallery', title: 'ギャラリー' },
-				{ href: '/scarlet-seal', title: '御朱印、仏前結婚式' },
-				{ href: '/multi-purpose-hall', title: '紫陽閣の貸し出し' },
-			],
-		},
-		{ href: '/events', title: '行事案内' },
-		{ href: '/cemetery', title: '墓地の案内' },
-		{ href: '/access', title: 'アクセス' },
-		{ href: '/contact', title: 'お問合せ' },
-	]
 	const isDesktop = useMediaQuery(`(min-width: ${BREAKPOINTS.TABLET}px)`)
 	const segment = useSelectedLayoutSegment()
+	const items = NAVIGATION_ITEMS
 
 	return isDesktop ? (
 		<MainMenuDesktop items={items} segment={segment} />
--- src/constants.ts
+++ src/constants.ts
@@ -15,3 +15,21 @@ export const BREAKPOINTS = {
 	TABLET: 768,
 	MOBILE: 375,
 }
+
+export const NAVIGATION_ITEMS = [
+	{ href: '/', title: 'Home' },
+	{
+		href: 'about',
+		title: '頼光寺について',
+		subMenu: [
+			{ href: '/about', title: 'ごあいさつ・沿革' },
+			{ href: '/gallery', title: 'ギャラリー' },
+			{ href: '/scarlet-seal', title: '御朱印、仏前結婚式' },
+			{ href: '/multi-purpose-hall', title: '紫陽閣の貸し出し' },
+		],
+	},
+	{ href: '/events', title: '行事案内' },
+	{ href: '/cemetery', title: '墓地の案内' },
+	{ href: '/access', title: 'アクセス' },
+	{ href: '/contact', title: 'お問合せ' },
+]
